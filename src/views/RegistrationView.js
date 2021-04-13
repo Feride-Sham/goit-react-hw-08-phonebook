e2f118097f0ad76025f78701b275d8b899cb0d76
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-04-13 20:38:16 +0300

    feat: add RegistrationView

--- src/views/RegistrationView.js
+++ src/views/RegistrationView.js
@@ -1,1 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
+
+class RegistrationView extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    this.setState({ name: "", email: "", password: "" });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { name, email, password } = this.state;
+    return (
+      <div>
+        <h1>Registration page</h1>
+        <form onSubmit={this.handleSubmit} autoComplete="off">
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Email
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegistrationView;

06fe39adfee21152eab89ebe519b59689f05d903
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-04-14 21:44:47 +0300

    feat: add Usermenu marKup

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-const UserMenu = () => {
-  return <div>usermenu</div>;
+const UserMenu = ({ avatar, name, onLogout }) => {
+  return (
+    <div>
+      <img src={avatar} alt="" width="48" />
+      <span>Welcome, {name}</span>
+      <button type="button" onClick={onLogout}>
+        Logout
+      </button>
+    </div>
+  );
 };
 export default UserMenu;

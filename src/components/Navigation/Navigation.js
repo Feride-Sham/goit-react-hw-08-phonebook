f1ab0b1371893d2305a1587e2285de6260652445
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-04-15 12:50:57 +0300

    feat: hides phone book until login

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import routes from "../../routes";
+import { authSelectors } from "../../redux/auth";
 
-const Navigation = () => {
+const Navigation = ({ isAuthenticated }) => {
   return (
     <nav>
       <NavLink to={routes.home}>HOME</NavLink>
-      <NavLink to={routes.contacts}>PHONEBOOK</NavLink>
+      {isAuthenticated && <NavLink to={routes.contacts}>PHONEBOOK</NavLink>}
     </nav>
   );
 };
 
-export default Navigation;
+const mapStateToProps = (state) => ({
+  isAuthenticated: authSelectors.getIsAuthenticated(state),
+});
+
+export default connect(mapStateToProps)(Navigation);

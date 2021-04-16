04172763ec1ee8ca1ba962c923cc0fb6a022e113
Feride Shamsutdinova <ferika.sham@gmail.com>
2021-04-16 16:12:40 +0300

    Updates

---
+++ static/js/home-view.1741fc1f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[1],{112:function(o,t,e){"use strict";e.r(t);e(0);var n=e(2);t.default=function(){return Object(n.jsx)("div",{children:Object(n.jsx)("h1",{children:"Hi! It's your phonebook"})})}}}]);
+//# sourceMappingURL=home-view.1741fc1f.chunk.js.map
